da9f6e263dc4683fbd308a88e997ba5828e3dcd7
Vladimir Barac <vladimir.barac32@gmail.com>
2018-10-16 14:46:41 +0200

    Added test_gen.sh

---
+++ test_gen.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+r_beg=1000
+r_end=5000
+
+h_beg=100
+h_end=10000
+
+interval1=5
+interval2=12
+
+r_leap=$(( r_end - r_beg ))
+h_leap=$(( h_end - h_beg ))
+
+r_leap=$( echo "scale=3; $r_leap * 1.0" | bc)
+h_leap=$( echo "scale=3; $h_leap * 1.0" | bc)
+
+r_leap=$( echo "scale=3; $r_leap / ($interval1 - 1)" | bc)
+h_leap=$( echo "scale=3; $h_leap / ($interval2 - 1)" | bc)
+
+echo "r_leap = $r_leap h_leap = $h_leap"
+
+r_iter=$r_beg
+h_iter=$h_beg
+no_iter=1
+
+while [ "$(echo "$r_iter <= $r_end" | bc)" -eq 1 ]; do
+    while [ "$(echo "$h_iter <= $h_end" | bc)" -eq 1 ]; do
+        h_print=$( echo $h_iter | awk '{print int($1+0.5)}' )
+        r_print=$( echo $r_iter | awk '{print int($1+0.5)}' )
+           
+        mkdir "input$no_iter"
+        echo $r_print > "input$no_iter/INPUT.TXT"
+        echo $h_print >> "input$no_iter/INPUT.TXT"
+                
+        h_iter=$( echo "scale=3; $h_iter + $h_leap" | bc )
+        no_iter=$(( $no_iter + 1 ))
+    done
+    r_iter=$( echo "scale=3; $r_iter + $r_leap" | bc)
+    h_iter=$h_beg
+done
