cc7e95c662e38c9e289bc5f41d0680cbe873a6d7
Vladimir Barac <vladimir.barac32@gmail.com>
2018-11-23 18:14:50 +0100

    Added python_model.py

---
+++ python_model.py
@@ -0,0 +1,53 @@
+import cv2
+import os 
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import RMSprop
+from sklearn.model_selection import train_test_split
+
+dir_imgs = '/home/amnesia/Desktop/1000_imgs/'
+
+dir_params = '/home/amnesia/Desktop/1000/'
+
+
+data = []
+params = []
+
+print("[INFO] loading images...")
+
+for i in range(0, 1000):
+    img_path = dir_imgs + "SLIKA" + str(i + 1) + ".png"
+    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    data.append(image)
+    param_path = dir_params + 'input' + str(i + 1) + '/INPUT.TXT'
+    params.append(np.loadtxt(param_path))
+
+data = np.array(data)
+params = np.array(params)
+
+print("[INFO] creating models")
+
+data_x = data[ : ,101, 100:201]
+trainX, testX, trainY, testY = train_test_split(data_x, params, test_size = 0.3, random_state = 42)
+
+model = Sequential()
+model.add(Dense(units = 101, activation = 'relu', input_dim = 101))
+model.add(Dense(units = 101, activation = 'relu'))
+model.add(Dense(units = 50, activation = 'relu'))
+model.add(Dense(units = 25, activation = 'relu'))
+model.add(Dense(units = 2))
+
+model.compile(loss='mse', optimizer=RMSprop(), metrics=["mean_absolute_error"])
+
+
+print('[INFO] training model...')
+
+model.fit(trainX, trainY, validation_split=0.2, epochs=30)
+
+
+loss_and_metrics = model.evaluate(testX, testY)
+
+print(loss_and_metrics)
+
+
