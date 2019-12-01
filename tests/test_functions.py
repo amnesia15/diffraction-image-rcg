45335b29e852c59d43726552a5f1cce5975b9cec
amnesia <amnesia15@users.noreply.github.com>
2019-12-01 17:03:32 +0100

    updated test_function paths

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -15,7 +15,9 @@ def dummy_image():
 @pytest.fixture
 def diffraction_image():
     path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                        '..\\images\\SLIKA1.png'))
+                                        '..',
+                                        'images',
+                                        'SLIKA1.png'))
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
     return image
@@ -24,7 +26,9 @@ def diffraction_image():
 @pytest.fixture
 def diffraction_image_top_ext():
     path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                        '..\\images\\SLIKA1.png'))
+                                        '..',
+                                        'images',
+                                        'SLIKA1.png'))
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     augmented_image = add_padding(image=image,
                                   side='top',
