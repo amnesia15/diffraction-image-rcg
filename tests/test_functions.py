b74423afa3582b6596f1e2f90a8ec84226f94519
amnesia <amnesia15@users.noreply.github.com>
2019-12-01 13:46:27 +0100

    Reorganized finding_center module and wrote unit test for its functions.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,36 @@
+import os
 import context
 import pytest
 import numpy as np
+import cv2
 from transform_image import add_padding
+from finding_center import find_center, prepare_model_image
 
 
 @pytest.fixture
 def dummy_image():
     return np.ones((201, 201))
+
+
+@pytest.fixture
+def diffraction_image():
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        '..\\images\\SLIKA1.png'))
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+    return image
+
+
+@pytest.fixture
+def diffraction_image_top_ext():
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        '..\\images\\SLIKA1.png'))
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    augmented_image = add_padding(image=image,
+                                  side='top',
+                                  number=20)
+
+    return augmented_image
 
 
 def test_add_padding(dummy_image):
@@ -34,3 +58,23 @@ def test_add_padding(dummy_image):
 
     assert np.all(augmented_image_left[:, 0:25] == 0)
     assert np.array_equal(augmented_image_left[:, 25:], dummy_image)
+
+
+def test_find_center(diffraction_image):
+    center_x, center_y = find_center(diffraction_image)
+
+    assert center_x == 100
+    assert center_y == 100
+
+
+def test_find_center_top_ext(diffraction_image_top_ext):
+    center_x, center_y = find_center(diffraction_image_top_ext)
+
+    assert center_x == 100
+    assert center_y == 120
+
+
+def test_prepare_model_image(diffraction_image, diffraction_image_top_ext):
+    image = prepare_model_image(diffraction_image_top_ext)
+
+    assert np.array_equal(image, diffraction_image)
